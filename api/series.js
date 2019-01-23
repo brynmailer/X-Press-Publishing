9e3aba159d64d2d5ecdd48bf5b2e905fa018840e
Bryn Mailer <bryndiupero@gmail.com>
2019-01-23 18:57:22 +1000

    added get route

--- api/series.js
+++ api/series.js
@@ -1,3 +1,18 @@
 const seriesRouter = require('express').Router();
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite');
+
+seriesRouter.get('/', (req, res, next) => {
+  db.all(
+    `SELECT *
+    FROM Series`,
+    (err, rows) => {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).json({ series: rows });
+    }
+  );
+});
 
 module.exports = seriesRouter;
--- migration.js
+++ migration.js
@@ -3,6 +3,21 @@ const db = new sqlite3.Database('./database.sqlite');
 
 db.serialize(() => {
   db.run(
+    `DROP TABLE IF EXISTS Artist`
+  );
+  db.run(
+    `CREATE TABLE Artist (
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      biography TEXT NOT NULL,
+      date_of_birth TEXT NOT NULL,
+      is_currently_employed INTEGER DEFAULT 1
+    )`
+  );
+});
+
+db.serialize(() => {
+  db.run(
     `DROP TABLE IF EXISTS Series`
   );
   db.run(
@@ -10,6 +25,24 @@ db.serialize(() => {
       id INTEGER PRIMARY KEY,
       name TEXT NOT NULL,
       description TEXT NOT NULL
+    )`
+  );
+});
+
+db.serialize(() => {
+  db.run(
+    `DROP TABLE IF EXISTS Issue`
+  );
+  db.run(
+    `CREATE TABLE Issue (
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      issue_number TEXT NOT NULL,
+      publication_date TEXT NOT NULL,
+      artist_id INTEGER NOT NULL,
+      series_id INTEGER NOT NULL,
+      FOREIGN KEY (artist_id) REFERENCES Artist(id),
+      FOREIGN KEY (series_id) REFERENCES Series(id)
     )`
   );
 });
